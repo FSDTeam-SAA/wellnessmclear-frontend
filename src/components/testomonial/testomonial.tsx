91e66e665731236192b72c3e3e268e7e03e29f68
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2025-07-01 15:56:06 +0600

    added

--- src/components/testomonial/testomonial.tsx
+++ src/components/testomonial/testomonial.tsx
@@ -17,6 +17,24 @@ const testimonials = [
       "The personalized wellness program transformed my approach to health. I've never felt more balanced and energized.",
   },
   {
+    name: "Sarah Johnson",
+    title: "Yoga Enthusiast",
+    image:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    rating: 5,
+    quote:
+      "The personalized wellness program transformed my approach to health. I've never felt more balanced and energized.",
+  },
+  {
+    name: "Sarah Johnson",
+    title: "Yoga Enthusiast",
+    image:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    rating: 5,
+    quote:
+      "The personalized wellness program transformed my approach to health. I've never felt more balanced and energized.",
+  },
+  {
     name: "Michael Chen",
     title: "Business Professional",
     image:
