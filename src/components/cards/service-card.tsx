99060cb92560319549d27387d241d0cfc597446e
Tanvirgit07 <ahmmedtanvir9090@gmail.com>
2025-07-02 17:34:41 +0600

    new add

--- src/components/cards/service-card.tsx
+++ src/components/cards/service-card.tsx
@@ -6,35 +6,54 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Brain } from "lucide-react";
 import Link from "next/link";
 
-const ServiceCard = () => {
+type ServiceCardProps = {
+  Icon: React.ComponentType<{ className?: string }>;
+  iconColor: string;
+  title: string;
+  description: string;
+  price: string | number;
+  buttonText: string;
+  href: string;
+  backgroundColor: string;
+};
+
+const ServiceCard = ({
+  Icon,
+  iconColor,
+  title,
+  description,
+  price,
+  href,
+  backgroundColor,
+}: ServiceCardProps) => {
   return (
-    <Card className="bg-[#F0F4F8] shadow-none rounded-[8px] max-w-[275px] ">
+    <Card
+      className={`${backgroundColor} shadow-none rounded-[8px] max-w-[275px] max-h-[305px]`}
+    >
       <CardHeader className="space-y-[28px]">
-        <Brain className="text-[#CBA0E3] size-[32px]" />
-        <CardTitle className="text-[18px] font-semibold ">
-          Clarity Health Audit
-        </CardTitle>
+        <Icon className={`${iconColor} size-[32px]`} />
+        <CardTitle className="text-[18px] font-semibold">{title}</CardTitle>
       </CardHeader>
 
       <CardContent className="text-[#424242] space-y-[16px]">
-        <p className="leading-[150%] font-normal text-[16px] ">
-          (1:1 Foundational Session – 60 mins)
-        </p>
-
+        <p className="leading-[150%] font-normal text-[16px]">{description}</p>
         <p>
           Price:{" "}
           <span className="text-[#131313] leading-[150%] font-semibold">
-            $150
+            {price}
           </span>
         </p>
       </CardContent>
-      <CardFooter>
-        <Button className="text-white w-full" asChild>
-          <Link href="/fsdfs">Book a Coach</Link>
+
+      <CardFooter className="flex items-center justify-between gap-2">
+        <Button className="text-white flex-1" asChild>
+          <Link href="/">Book A Coach</Link>
         </Button>
+        <button className="text-[#CBA0E3] flex-1">
+          <Link href={href}>See Details</Link>
+        </button>
       </CardFooter>
     </Card>
   );
