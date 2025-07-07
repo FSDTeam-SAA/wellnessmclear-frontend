a6cd25143a734d21efec041f507abb214df13d3a
ILhankhondaker <ilhankhondaker@gmail.com>
2025-07-07 13:53:07 +0600

    add sighnup link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -202,7 +202,7 @@ export function Navbar() {
                     </li>
                     <li>
                       <Link
-                        href="/signup"
+                        href="/sign-up"
                         className="block px-4 py-2 hover:bg-gray-500/10 text-black"
                         onClick={() => setIsDropdownOpen(false)}
                       >
@@ -211,7 +211,7 @@ export function Navbar() {
                     </li>
                     <li>
                       <Link
-                        href="/signup"
+                        href="/sign-up"
                         className="block px-4 py-2 hover:bg-gray-500/10 text-red-600"
                         onClick={() => setIsDropdownOpen(false)}
                       >
